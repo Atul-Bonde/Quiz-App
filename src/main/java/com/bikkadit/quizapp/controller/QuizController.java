66bf9fc16f0552771a63de16e9eadac554ffcfa5
Atul-Bonde <139678114+Atul-Bonde@users.noreply.github.com>
2023-09-21 16:32:41 +0530

    Update QuizController.java

--- src/main/java/com/bikkadit/quizapp/controller/QuizController.java
+++ src/main/java/com/bikkadit/quizapp/controller/QuizController.java
@@ -42,6 +42,11 @@ public class QuizController {
 
 		return new ResponseEntity<List<QuestionMapper>>(this.quizService.getQuiz(quizId), HttpStatus.OK);
 	}
+	@GetMapping("/quiz/{quizId2}")
+	public ResponseEntity<List<QuestionMapper>> getQuiz(@PathVariable Long quizId) {
+
+		return new ResponseEntity<List<QuestionMapper>>(this.quizService.getQuiz(quizId), HttpStatus.OK);
+	}
 
 	@GetMapping("/quizresult/{quizId}")
 	public ResponseEntity<Integer> resultQuiz(@PathVariable long quizId, @RequestBody List<Response> responses) {
